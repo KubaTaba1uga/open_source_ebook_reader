4824a55803d121d8c33990cffc52fbd07c0afd14
KubaTaba1uga <luna7337anul@proton.me>
2026-05-16 12:19:10 +0200

    init repo

---
+++ docs/index.md
@@ -0,0 +1,24 @@
+Open Source eBook Reader
+=================================
+
+% TO-DO: Add a picture of the device here
+
+This is open source ebook reader project. 
+Goal of the project is to allow users to hack their readers easily.
+
+I have a kobo reader which is pretty cool, but lack some features which are realtively easy to implement on linux, like words translations between languages in pdfs, or auto syncing books on the device with git. The idea is to create a platform which integrates Linux supported CPU, epaper display, buttons, battery and an enclosure, and on top of the platform build ebook reader app, which can be easily customized.
+
+If you just need an ebook reader, going with Kobo is better choice for 99% of use cases, but if you are a programmer with some wierd ideas how would you like to use the reader, feel free to continue. Building the device itself is pretty time consuming, explaining it all and documenting would require a lot of efforts wich i would rather put into improving the software, so at the moment only channel to aquire the device is to buy it. If you are interested in buying the reader just email me directly, and i will build one for you.
+
+% TO-DO: insert email for ebook reader orders, sth like openSourceEbook4Sell@proton.me
+
+The documentation starts from getting started page, so if you are new to the reader this is where I advice to begin.
+
+```{toctree}
+:maxdepth: 2
+
+getting_started
+```
+
+
+
---
+++ tasks.py
@@ -0,0 +1,100 @@
+"""
+SPDX-License-Identifier: MIT
+
+Copyright 2026 Jakub Buczynski <KubaTaba1uga>
+"""
+
+import os
+
+from invoke import task
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+BUILD_PATH = os.path.join(ROOT_PATH, "build")
+DOCS_PATH = os.path.join(ROOT_PATH, "docs")
+PACKAGES_PATH = os.path.join(ROOT_PATH, "package")
+
+C_FORMATER = "clang-format-19"
+C_LINTER = "clang-tidy-19"
+
+os.environ["PATH"] = f"{os.path.join(ROOT_PATH, '.venv', 'bin')}:{os.environ['PATH']}"
+os.chdir(ROOT_PATH)
+
+
+@task
+def deps_install(c):
+    _pr_info(f"Installing dependencies...")
+
+    try:
+        c.run(
+            "sudo apt-get install -y doxygen virtualenv git python3"
+            f" {C_FORMATER} {C_LINTER} "
+        )
+
+        c.run("virtualenv .venv")
+        c.run(
+            "pip install invoke sphinx==8.2.3 breathe==4.36.0 sphinx_rtd_theme==3.0.2 sphinx-autobuild==2025.08.25"
+        )
+
+    except Exception:
+        _pr_error("Installing failed")
+        raise
+
+    _pr_info(f"Installing dependencies completed")
+
+
+@task
+def docs_build(c):
+    _pr_info("Building docs...")
+
+    docs_path = os.path.join(BUILD_PATH, "docs", "html")
+
+    try:
+        c.run("doxygen docs/Doxyfile")
+        with c.cd(DOCS_PATH):
+            c.run(f"sphinx-build -b html . {docs_path}")
+    except Exception:
+        _pr_error(f"Building docs failed")
+        raise
+
+    _pr_info("Building docs completed")
+
+
+@task
+def docs_serve(c, port=8000):
+    _pr_info("Serving docs...")
+
+    docs_build(c)
+
+    c.run(
+        " ".join(
+            [
+                f"sphinx-autobuild",
+                f"--port {port}",
+                f"docs build/docs/html",
+            ]
+        ),
+        pty=True,
+    )
+
+    _pr_info("Serving docs completed")
+
+
+###############################################
+#                Private API                  #
+###############################################
+def _pr_info(message: str):
+    print(f"\033[94m[INFO] {message}\033[0m")
+
+
+def _pr_warn(message: str):
+    print(f"\033[93m[WARN] {message}\033[0m")
+
+
+def _pr_debug(message: str):
+    print(f"\033[96m[DEBUG] {message}\033[0m")
+
+
+def _pr_error(message: str):
+    print(f"\033[91m[ERROR] {message}\033[0m")
+
+
