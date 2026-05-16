a99e1c0905bd407f99abc539e84516f8383e4c4f
KubaTaba1uga <luna7337anul@proton.me>
2026-05-16 16:58:37 +0200

    add app with deps

---
+++ package/ebook_reader/ebook_reader.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# Ebook reader app
+#
+################################################################################
+
+EBOOK_READER_VERSION = v0.0.1
+EBOOK_READER_SITE = $(BR2_EXTERNAL_EBK_READER_PATH)/package/ebook_reader
+EBOOK_READER_SITE_METHOD = local
+EBOOK_READER_DEPENDENCIES = it8951_epaper lvgl host-pkgconf
+EBOOK_READER_INSTALL_STAGING = YES
+
+$(eval $(meson-package))
+
